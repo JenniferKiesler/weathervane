93938f7bb1b0a734632b6764079f2832da0c1418
JKies <jennyhawes24@gmail.com>
2023-09-25 11:38:15 -0500

    submits city name

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import SearchForm from './components/SearchForm';
 import CurrentWeather from './components/CurrentWeather';
 import WeatherForecast from './components/WeatherForecast';
+import React, {useState} from 'react';
 
 function App() {
+  const [cityInfo, setCityInfo] = useState({})
+
+  const getCurrentWeather = (city) => {
+    console.log(city)
+  }
+
   return (
     <>
       <header>
         <h1>WeatherVane</h1>
       </header>
       <main>
-        <SearchForm/>
+        <SearchForm
+          getCurrentWeather={getCurrentWeather}
+        />
         <CurrentWeather/>
         <WeatherForecast/>
       </main>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,44 @@
-const SearchForm = ({}) => {
+import React, {useState} from 'react';
+
+const SearchForm = ({ getCurrentWeather }) => {
+  const [inputValue, setInputValue] = useState("")
+
+  const handleChange = (event) => {
+    const { value } = event.target
+    setInputValue(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    try {
+      let cityInput = inputValue.toLowerCase();
+      let individualWords = cityInput.split(" ")
+  
+      for (let i = 0; i < individualWords.length; i++) {
+        individualWords[i] = individualWords[i][0].toUpperCase() + individualWords[i].substring(1);
+      }
+  
+      cityInput = individualWords.join(" ")
+      setInputValue(cityInput)
+
+      getCurrentWeather(cityInput)
+      setInputValue("")
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <div>Search Form</div>
+    <form id="search-form" onSubmit={handleSubmit}>
+      <label htmlFor="search-input">Search for a City:</label>
+      <input 
+        type="search" 
+        id="search-input"
+        name='search'
+        value={inputValue}
+        onChange={handleChange}/>
+      <button type="submit" id="search-button">Search</button>
+    </form>
   )
 }
 
